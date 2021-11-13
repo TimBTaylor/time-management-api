bd9f67ab181dc86b932f36a04b841bce52625a87
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2021-11-12 20:12:16 -0500

    revert database connection

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-const db = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DATABASE_HOSTNAME,
   port: 3306,
   user: process.env.DATABASE_USER,
@@ -9,13 +9,13 @@ const db = mysql.createPool({
   database: process.env.DATABASE_NAME,
 });
 
-// db.connect((err) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   } else {
-//     console.log("database connected");
-//   }
-// });
+db.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    console.log("database connected");
+  }
+});
 
 module.exports = db;
